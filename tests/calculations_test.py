958c7237633e532fb1669fa32b0562d357a68c3c
msousapenha <144005239+msousapenha@users.noreply.github.com>
2026-01-09 16:40:21 -0300

    fix identation

--- tests/calculations_test.py
+++ tests/calculations_test.py
@@ -66,7 +66,7 @@ def test_area_of_circle_negative_radius():
     with pytest.raises(ValueError):
         area_of_circle(radius)
 
- def test_get_nth_fibonacci_negative():
+def test_get_nth_fibonacci_negative():
     """Test with a negative number to raise ValueError."""
     # Arrange
     n = -1
